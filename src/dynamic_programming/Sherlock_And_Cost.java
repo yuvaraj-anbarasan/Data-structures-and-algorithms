0e047ec2dbcc7dcc6bc7bc3315a38f1ef01cc4cc
yuvaraj-anbarasan <yuvaraj.klei@gmail.com>
2019-10-29 22:14:16 +0530

    Solution for sherlock & cost problem

--- src/dynamic_programming/Sherlock_And_Cost.java
+++ src/dynamic_programming/Sherlock_And_Cost.java
@@ -4,15 +4,24 @@ https://www.hackerrank.com/challenges/sherlock-and-cost/problem
 */
 public class Sherlock_And_Cost {
     static int max(int[] ar){
-        int sum=0;
+        int max1=0, max2=0;
         for(int i=1; i<ar.length; i++){
-            sum += (Math.abs(ar[i] -ar[i-1]));
+            if(i%2 != 0) {
+                max1 += (Math.abs(ar[i] - 1));
+                max2 += (Math.abs(1 - ar[i-1]));
+            }
+            else {
+                max1 += (Math.abs(1 - ar[i-1]));
+                max2 += (Math.abs(ar[i] - 1));
+            }
+
+            System.out.println("max1="+max1+" | max2="+max2);
         }
 
-        return sum;
+        return Math.max(max1, max2);
     }
     public static void main(String[] args){
-        int[] ar = {10 ,1 ,10 ,1 ,10};
+        int[] ar = {30,1,1,30};
         System.out.println(max(ar));
     }
 }
